106cb2e3c1d6f2a61e226ad1d8ba20c9538fe1c6
Forek145 <pilarczyk.miko@gmail.com>
2026-04-19 00:45:29 +0200

    ostateczna wersja stylu

--- src/footer.tsx
+++ src/footer.tsx
@@ -1,4 +1,3 @@
-import { Mic } from "lucide-react";
 import {Link} from "react-router-dom";
 
 export function Footer()
@@ -8,9 +7,7 @@ export function Footer()
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
             <div className="flex items-center space-x-2">
-              <div className="w-8 h-8 bg-gradient-to-br from-purple-500 to-emerald-400 rounded-lg flex items-center justify-center">
-                <Mic className="w-5 h-5 text-white" />
-              </div>
+             <img src={"/ARU_logo.png"} className={"w-8"}/>
               <span className="bg-gradient-to-r from-cyan-400 to-green-300 bg-clip-text text-transparent">
                 Aru Record
               </span>
